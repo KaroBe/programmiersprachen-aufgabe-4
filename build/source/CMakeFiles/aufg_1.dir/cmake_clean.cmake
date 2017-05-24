2aaac83995af9abbddf28db10ebaf3bbc241a429
Karoline Brehm <karoline.engel.brehm@uni-weimar.de>
2017-05-24 17:14:26 +0200

    list.hpp copied for 3.2

---
+++ build/source/CMakeFiles/aufg_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufg_1.dir/aufg_1.cpp.o"
+  "aufg_1.pdb"
+  "aufg_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufg_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
